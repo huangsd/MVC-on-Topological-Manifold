93c89a307d1b6d1198f0525483f0ce3090299432
Shudong Huang (黄树东) <huangsd@scu.edu.cn>
2022-11-03 16:35:55 +0800

    Delete run_MVCtoplogic.m

--- run_MVCtoplogic.m
+++
@@ -1,8 +0,0 @@
-clear 
-close all
-para_alpha = [0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10];
-para_beta = [0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10]; 
-load('3Sources.mat'); 
-% res = [acc, nmi, Pu, Fscore, Precision, Recall, ARI];
-[result, S, Tim, Obj] = MVCtoplogic(data,labels, para_alpha(8), para_beta(7));
-
